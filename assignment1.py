954e60733c50de2422309a3d07c5cfbc337f0c44
Shree24 <smailybhatnagar@gmail.com>
2018-08-08 12:42:55 +0530

    First assignment

---
+++ assignment1.py
@@ -0,0 +1,17 @@
+#QUESTION 1
+
+x=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+print(x[7:12])                                 # slicing from the front
+print(x[-9:-4])                                # slicing from back
+#END
+
+# Question 2
+
+x=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+print(x[1::2])                                 # print even numbers from list using slicing
+#END
+# Question 3
+
+x=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]     # print every fourth no. using slicing
+print(x[0::4])
+#END
